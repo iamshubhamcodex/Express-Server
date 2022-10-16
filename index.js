3741aa90c50f3b68dafa97fd6dc11d25347bbfb7
Shubham Singh <shubhamsinghcodex@gmail.com>
2022-10-16 21:02:59 +0530

    Changed PORT according to heroku

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-const port = 5500;
+const port = process.env.PORT || 5500;
 let tim = 0;
 
 app.use(express.json());
 app.use(cors());
 
 app.use("/api", (req, res) => {
-  res.json(tim);
   tim++;
+  res.json(tim);
 });
 
 app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}`);
+  console.log(`Listening at ${port} port`);
 });
